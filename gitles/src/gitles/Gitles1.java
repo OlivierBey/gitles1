ed52546cce826245b4a45f3b7d26a27182be143c
OlivierBey <olivier.beyaert@telenet.be>
2022-11-15 11:07:16 +0100

    oefening1

--- gitles/src/gitles/Gitles1.java
+++ gitles/src/gitles/Gitles1.java
@@ -4,7 +4,7 @@ public class Gitles1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("gitles");
+System.out.println("Oefening1 van Github");
 	}
 
 }
